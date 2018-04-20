9a2029a689f0a4750340c7b7cec18c54d56c2046
Yaser Almasri <yaser@masys.co>
2018-04-20 15:27:43 -0500

    require exmples and support files

--- lib/rspec_helpers.rb
+++ lib/rspec_helpers.rb
@@ -1,5 +1,10 @@
-require "rspec_helpers/version"
+require 'rspec_helpers/version'
+Dir[
+  File.join(__dir__, 'rspec_helpers', 'examples', '*.rb'),
+  File.join(__dir__, 'rspec_helpers', 'support', '*.rb')
+].each do |file|
+  require file
+end
 
 module RspecHelpers
-  # Your code goes here...
 end
