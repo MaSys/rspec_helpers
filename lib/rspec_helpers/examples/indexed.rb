c0c5ba626e4c0193f915d24341012030440fa8c6
Yaser Almasri <yaser@masys.co>
2018-04-22 09:42:56 -0500

    fix polymorphic columns

--- lib/rspec_helpers/examples/indexed.rb
+++ lib/rspec_helpers/examples/indexed.rb
@@ -2,9 +2,21 @@ RSpec.shared_examples 'indexed' do |excluded_columns|
   # Indexes
   excluded_columns = [excluded_columns] unless excluded_columns.is_a? Array
 
+  relations = described_class.reflect_on_all_associations :belongs_to
+  relations.each do |relation|
+    relation_name = relation.name.to_s
+    column = relation_name + '_id'
+    if relation.options[:polymorphic]
+      it { is_expected.to have_db_index([:"#{relation_name}_type", column]) }
+    else
+      it { is_expected.to have_db_index(column) }
+    end
+  end
+
   described_class.column_names.each do |c|
     next if excluded_columns.include? c
     next if c.ends_with?('able_id')
+    next if described_class.column_names.include?("#{c.gsub('_id', '')}_type")
     next unless c.ends_with?('_id')
     it { is_expected.to have_db_index(c.to_sym) }
   end
