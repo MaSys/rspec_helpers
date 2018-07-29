a00b37e10a7c6e372f451b46f0a1b60a1a510924
Yaser Almasri <yaser@masys.co>
2018-07-29 10:23:49 -0500

    use to_sql

--- lib/rspec_helpers/examples/paranoia.rb
+++ lib/rspec_helpers/examples/paranoia.rb
@@ -5,11 +5,11 @@ RSpec.shared_examples 'a Paranoid model' do
     it { is_expected.to have_db_index(:deleted_at) }
 
     it 'adds a deleted_at where clause' do
-      expect(described_class.all).to include '"deleted_at" IS NULL'
+      expect(described_class.all.to_sql).to include '"deleted_at" IS NULL'
     end
 
     it 'skips adding the deleted_at where clause when unscoped' do
-      expect(described_class.unscoped.to_s)
+      expect(described_class.unscoped.to_sql)
         .to_not include 'deleted_at'
     end
   end
