8559163b9c6b1820e4d2cc14532ebfa57665a729
Yaser Almasri <yaser@masys.co>
2019-07-18 13:20:32 -0500

    fix has many specs

--- lib/rspec_helpers/examples/inverse_of.rb
+++ lib/rspec_helpers/examples/inverse_of.rb
@@ -23,15 +23,29 @@ RSpec.shared_examples 'inverse of association' do |excluded|
     it relation.name.to_s do
       expect(relation.options[:inverse_of]).to_not be nil
 
-      if relation.options[:through]
-        expect(relation.options[:inverse_of]).to eq(
-          described_class.name.pluralize.underscore.to_sym)
-      elsif relation.options[:class_name]
-        expect(relation.options[:class_name].underscore.to_sym).to eq(
-          described_class.name.underscore.to_sym)
+      if relation.options[:class_name]
+        klass = relation.options[:class_name].to_s.singularize.classify.constantize
+        rels = klass.reflect_on_all_associations(:belongs_to)
+        rel = rels.detect do |r|
+          r.name == relation.options[:inverse_of]
+        end
+        expect(rel).to_not be nil
+
+      elsif relation.options[:through]
+        klass = relation.name.to_s.singularize.classify.constantize
+        rels = klass.reflect_on_all_associations(:has_many)
+        rel = rels.detect do |r|
+          r.name == relation.options[:inverse_of]
+        end
+        expect(rel).to_not be nil
+
       else
-        expect(relation.options[:inverse_of]).to eq(
-          described_class.name.underscore.to_sym)
+        klass = relation.name.to_s.singularize.classify.constantize
+        rels = klass.reflect_on_all_associations(:belongs_to)
+        rel = rels.detect do |r|
+          r.name == relation.options[:inverse_of]
+        end
+        expect(rel).to_not be nil
       end
     end
   end
